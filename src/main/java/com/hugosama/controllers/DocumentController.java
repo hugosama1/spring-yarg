8f4894caaa20ce4a4529048dbdfcec4fea55bf7c
hugosama <hugosama1@gmail.com>
2017-08-29 20:32:27 -0600

    Functional example

--- src/main/java/com/hugosama/controllers/DocumentController.java
+++ src/main/java/com/hugosama/controllers/DocumentController.java
@@ -7,9 +7,7 @@ package com.hugosama.controllers;
 
 import com.haulmont.yarg.formatters.factory.DefaultFormatterFactory;
 import com.haulmont.yarg.loaders.factory.DefaultLoaderFactory;
-import com.haulmont.yarg.loaders.impl.GroovyDataLoader;
 import com.haulmont.yarg.loaders.impl.JsonDataLoader;
-import com.haulmont.yarg.reporting.ReportOutputDocument;
 import com.haulmont.yarg.reporting.Reporting;
 import com.haulmont.yarg.reporting.RunParams;
 import com.haulmont.yarg.structure.Report;
@@ -17,60 +15,42 @@ import com.haulmont.yarg.structure.ReportBand;
 import com.haulmont.yarg.structure.ReportOutputType;
 import com.haulmont.yarg.structure.impl.BandBuilder;
 import com.haulmont.yarg.structure.impl.ReportBuilder;
-import com.haulmont.yarg.structure.impl.ReportFieldFormatImpl;
 import com.haulmont.yarg.structure.impl.ReportTemplateBuilder;
-import com.haulmont.yarg.util.groovy.DefaultScriptingImpl;
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DocumentController {
-    
-    @RequestMapping(path="/generate/doc",method = RequestMethod.GET)
-    public String generateDocument() throws IOException {
-            ReportBuilder reportBuilder = new ReportBuilder();
+
+    @RequestMapping(path = "/generate/doc", method = RequestMethod.GET)
+    public void generateDocument(HttpServletResponse response) throws IOException {
+        ReportBuilder reportBuilder = new ReportBuilder();
         ReportTemplateBuilder reportTemplateBuilder = new ReportTemplateBuilder()
-                .documentPath("./src/main/resources/invoice.docx")
-                .documentName("invoice.docx")
+                .documentPath("./src/main/resources/Letter.docx")
+                .documentName("Letter.docx")
                 .outputType(ReportOutputType.docx)
                 .readFileFromPath();
         reportBuilder.template(reportTemplateBuilder.build());
         BandBuilder bandBuilder = new BandBuilder();
-        String json = "{ \"main\" : " +
-                "                              { " +
-                "                               \"invoiceNumber\":99987" +
-                "                            }}";
-        System.out.println(json);
-        ReportBand main = bandBuilder.name("Main").query("Main", json, "json", "parameter=param1 &.main").build();
-
-
-        bandBuilder = new BandBuilder();
-        ReportBand items = bandBuilder.name("Items").query("Items", "return [\n" +
-                "                                ['name':'Java Concurrency in practice', 'price' : 15000],\n" +
-                "                                ['name':'Clear code', 'price' : 13000],\n" +
-                "                                ['name':'Scala in action', 'price' : 12000]\n" +
-                "                            ]", "groovy").build();
+        String json = FileUtils.readFileToString(new File("./src/main/resources/Data.json"));
+        ReportBand main = bandBuilder.name("Main").query("Main", "parameter=param1 $.main", "json").build();
 
         reportBuilder.band(main);
-        reportBuilder.band(items);
-        reportBuilder.format(new ReportFieldFormatImpl("Main.signature", "${html}"));
-        reportBuilder.format(new ReportFieldFormatImpl("Main.footer", "${html}"));
-
         Report report = reportBuilder.build();
 
         Reporting reporting = new Reporting();
         reporting.setFormatterFactory(new DefaultFormatterFactory());
         reporting.setLoaderFactory(
-                new DefaultLoaderFactory().setGroovyDataLoader(new GroovyDataLoader(new DefaultScriptingImpl()))
-        .setJsonDataLoader(new JsonDataLoader()));
+                new DefaultLoaderFactory()
+                        .setJsonDataLoader(new JsonDataLoader()));
 
-        ReportOutputDocument reportOutputDocument = reporting.runReport(
-                new RunParams(report).param("param1", json), new FileOutputStream("./src/main/resources/output.docx"));
-        
-        return "{}";
+        response.setContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        reporting.runReport(new RunParams(report).param("param1", json), response.getOutputStream());
     }
-    
+
 }
